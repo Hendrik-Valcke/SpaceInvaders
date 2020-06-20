2d856b323060676813bd9e93abe170efe60fa75c
Hendrik Valcke <hendrik.valcke@hotmail.be>
2020-06-20 22:49:49 +0200

    help2

---
+++ GameV0_1/Resources/Config.h
@@ -0,0 +1,30 @@
+//
+// Created by Hendrik Valcke on 20/06/2020.
+//
+
+#ifndef GAMEV0_1_CONFIG_H
+#define GAMEV0_1_CONFIG_H
+
+
+
+//hier komen alle contanten
+
+const int SCREEN_W = 1024;
+const int SCREEN_H = 768;
+
+const int ENEMIES_PER_ROW = 6;
+const int ENEMY_W = 64;
+const int ENEMY_H = 64;
+const int ENEMY_X_START=64;
+const int ENEMY_Y_START=64;
+const int ENEMY_SPEED = 1;
+
+const int MAX_HEALTH = 3;
+const int PLAYER_H=64;
+const int PLAYER_W=64;
+const int PLAYER_SPEED = 20;
+
+const int FRAMES_PER_SEC=60;
+
+
+#endif //GAMEV0_1_CONFIG_H
--- GameV0_1/main.cpp
+++ GameV0_1/main.cpp
@@ -2,12 +2,6 @@
 #include "Resources/Game.h"
 #include "Resources/SDL/SdlFactory.h"
 
-/*<<<<<<< Updated upstream
-int main(int argc, const char* argv[]) {
-    Game* myGame= new Game{new SdlFactory()};
-    //myGame->runScreen();
-    myGame->startGame();
-=======*/
 int main(int argc, char* argv[])
 {
     Game* SpaceInvaders = new Game{new SdlFactory()};
